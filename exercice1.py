aa0f8f08354c332619563573eeb0213bd2fcd822
pltguest <pltguest@ad.polytech.sorbonne-universite.fr@pc4174-linux.ad.polytech.sorbonne-universite.fr>
2026-09-15 16:54:11 +0200

    first commit

---
+++ exercice1.py
@@ -0,0 +1,9 @@
+
+
+releve1 = ("laser_avant", 2.35, "m")
+releve2 = ("laser_arriere", 1.10, "m")
+releve3 = ("gyroscope", 87.5, "deg")
+releves = [releve1, releve2, releve3]
+
+def afficher_releve(releves):
+    
