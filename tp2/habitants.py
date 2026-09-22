05a8a297120fee382585709dfd9c8577fd848f28
pltguest <pltguest@ad.polytech.sorbonne-universite.fr@pc4174-linux.ad.polytech.sorbonne-universite.fr>
2026-09-22 15:01:45 +0200

    exercice 1

---
+++ tp2/habitants.py
@@ -0,0 +1,19 @@
+class Habitant:
+    def __init__(self, nom : str, age : int, adresse : str, animaux : dict = None):
+        self.nom = nom
+        self.age = age 
+        self.adresse = adresse
+        self.animaux = animaux if animaux is not None else {}
+
+    def affichage_adresse (self) : 
+        print(f"{self.nom} habite à {self.adresse}" )
+
+    def compte_animal(self, animal):
+        return self.animaux.get(animal, 0)
+    
+h1 = Habitant("Aldric", 25, "Rue A", {"vaches": 3})
+assert h1.nom == "Aldric"
+assert h1.compte_animal("vaches") == 3
+assert h1.compte_animal("moutons") == 0
+h1.affichage_adresse() # affiche "Aldric habite a Rue A"
+    
