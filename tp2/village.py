07ef83941c1fd59811a5308d91b44d735c088516
pltguest <pltguest@ad.polytech.sorbonne-universite.fr@pc4174-linux.ad.polytech.sorbonne-universite.fr>
2026-09-22 16:26:52 +0200

    exercice 5 : village

---
+++ tp2/village.py
@@ -0,0 +1,17 @@
+class village :
+    def __init__(self, nom) : 
+        self.nom = nom
+        self.habitant = []
+
+    def ajouter_habitant_composition(self, nom, age, adresse, animaux = None):
+        nouvel_habitant = habitant ( nom, age, adresse, animaux)
+        self.habitant.append(nouvel_habitant)
+
+    def ajouter_habitant_agregation(self, habitant):
+        self.habitant.append(habitant)
+
+
+#on peut voir que sur la fonction de composition cette derniere illustre bien le principe
+#car on peut voir la relation forte, les objets sont liés
+#tandis que pour la fonction d'agregation on observe bien que l'objet de la fonction
+#peut etre utilise de maniere independante
